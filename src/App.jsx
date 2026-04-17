e54fa94e03d9497ae7c378cb6733a2e0b52b4e88
NataliiaProkopenko87 <nataliiaprokopenko@yahoo.com>
2026-04-17 14:55:36 +0200

    Update App.jsx
    Added class is-hidden -mobile for images

--- src/App.jsx
+++ src/App.jsx
@@ -13,11 +13,11 @@ function App() {
           <div className="container">
          <header className="mb-6 has-text-centered">
           <h1 className="title is-2" style={{fontWeight: 400, letterSpacing: '-0.02em', color: '#243370'}}>
-            <span className='m-4'>
+            <span className='m-4 is-4by3 is-hidden-mobile'>
               <img src={bigBen} alt="Big Ben " style={{ width: '100%', maxWidth: '100px', borderRadius: '8px' }} />
             </span>
             Great Britain
-            <span className='m-4'>
+            <span className='m-4 is-4by3 is-hidden-mobile'>
               <img src={gbFlag} alt="Britain Flag" style={{ width: '100%', maxWidth: '100px', borderRadius: '8px' }} />
               </span>
               </h1>
